e630f4ad93b2856ef5f8716c5ac73f1b3a1a848e
ChintaNandiniDevi <84563282+ChintaNandiniDevi@users.noreply.github.com>
2022-03-30 23:19:54 +0530

    Add files via upload
    spring

---
+++ forestSpring/src/main/java/com/cg/ForestSpringApplication.java
@@ -0,0 +1,13 @@
+package com.cg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForestSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForestSpringApplication.class, args);
+	}
+
+}
